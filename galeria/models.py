4e7fe91756e9244b6c7e4358bb9645fec21b2d6a
rafaelmarquespn <rafaelmarquespn>
2023-01-26 13:47:42 -0300

    atualizaçao

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import datetime
+class Fotografia(models.Model):
 
-# Create your models here.
+    OPCOES_CATEGORIA = [
+        ("NEBULOSA","nebulosa"),
+        ("ESTRELA", "estrela"),
+        ("GALAXIA", "galaxia"),
+        ("PLANETA","planeta"),
+    ]
+
+    nome = models.CharField(max_length=100, null =False, blank=False)
+    legenda = models.CharField(max_length=150, null =False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+    categoria = models.CharField(max_length=100,choices=OPCOES_CATEGORIA, default='')
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
+
+    def __str__(self) -> str:
+        return f"Fotografia [nome={self.nome}]"
